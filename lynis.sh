e9676743d5553080ca5a75f20f5bd3d00fb76174
Brady <bwynkoop47@gmail.com>
2026-02-27 14:34:22 -0500

    Update lynis.sh

--- lynis.sh
+++ lynis.sh
@@ -1,28 +1,23 @@
 #!/usr/bin/env bash
 ##!/bin/bash
 
-######## Bash Detailed Linux System Audits Using LYNIS #############
+###### Simple LYNIS Script For System Hardening Documentation ######
 ####################################################################
-# bin/bash - Runs Only If Located In Specified Path		   #
+# bin/bash - Runs Only If Located In Specified Path                #
 # usr/bin/env bash - Any Bash In User Path (Respects Custom Paths) #
-# && Allows Run Text To Only Display Printf If The Command Runs    #
 ####################################################################
 
 # Runs Lynis Audit Tool With Sudo Privileges 
 sudo lynis audit system &&
 
-# Grabs Warnings & Suggestions from Lynis audit tool and sends it to lynis-review.txt
+# Grabs Warnings & Suggestions from Lynis and sends it to lynis-review.txt 
 sudo grep -E "warning|suggestion" /var/log/lynis-report.dat > lynis-review.txt &&
 
-# Opens the Generated Report In The Terminal Afterwards
+# Opens the Generated Report In a text editor
 open lynis-review.txt 
 
-##################################
-# Printf > Echo			 #
-# Printf = Versatile & Control   #
-# Echo = Bash & Control		 #
-#       ##########################
-#-Brady #
+#########
+# Brady #
 #########
 
 # * CREDITS TO LYNIS AUDITING TOOL *
